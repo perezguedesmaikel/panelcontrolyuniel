6c9d6b167b9f4c5a176f8b5dbc95913e7a223829
Maikel33 <perezguedesmaikel@gmail.com>
2022-06-27 13:03:48 -0400

    context creado

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React,{useState} from 'react';
+import React from 'react';
 import './App.css';
 import SignIn from "./components/login";
 import {Routes,Route} from 'react-router-dom';
 import Items from "./components/items";
 import Registrar from "./components/Registrar";
+import {AuthProvider} from './context/authContext';
 function App() {
 
   return (
     <div className="App">
+        <AuthProvider>
         <Routes>
             <Route path='/' element={<SignIn/>}/>
             <Route path='/registrar' element={<Registrar/>}/>
             <Route path='/items' element={<Items/>}/>
 
         </Routes>
-
+        </AuthProvider>
     </div>
   );
 }
---
+++ src/context/authContext.js
@@ -0,0 +1,12 @@
+import React, {createContext} from 'react';
+const context=createContext({});
+export function AuthProvider({children}) {
+    const user={
+        login:true
+    }
+    return(
+        <context.Provider value={{user}}>
+            {children}
+        </context.Provider>
+    )
+}
